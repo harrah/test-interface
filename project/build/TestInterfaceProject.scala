5c360f21f002d6e843e4872dc3178b5e525bbfe9
Mark Harrah <dmharrah@gmail.com>
2010-01-10 22:02:42 -0500

    bump versions, add publishing configuration

--- project/build/TestInterfaceProject.scala
+++ project/build/TestInterfaceProject.scala
@@ -3,4 +3,7 @@ import sbt._
 class TestInterfaceProject(info: ProjectInfo) extends DefaultProject(info)
 {
 	override def mainJavaSourcePath = path("src")
+	override def managedStyle = ManagedStyle.Maven
+	val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+	Credentials(Path.userHome / ".ivy2" / ".credentials", log)
 }
