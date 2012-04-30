6dd5b0b14171372ec2b7c9e26946650f2944e926
bvenners <bill@artima.com>
2012-04-29 21:56:16 -0400

    Delete the old Event hierarchy of classes and rename the Event2 interface to Event.

--- src/org/scalasbt/testing/Event.java
+++ src/org/scalasbt/testing/Event.java
@@ -1,63 +1,38 @@
 package org.scalasbt.testing;
 
 /**
- * An Event fired by the test framework during a run.
+ * An event fired by the test framework during a run.
  */
-public abstract class Event {
+public interface Event {
 
-    private String fullyQualifiedName;
-    private boolean isModule;
-    private Selector selector;
-
-    /**
-     * Constructs a new <code>Event</code> with passed fully qualified name, isModule flag, and selector.
-     *
-     * @param fullyQualifiedName the fully qualified name of a class that can rerun the suite or test
-     *                           about which an event was fired
-     * @param isModule indicates whether the fullyQualifiedName refers to a module (singleton object). If so, the
-     *          fullyQualifiedName parameter does not include the trailing dollar sign.
-     * @param selector additional information identifying the suite or test about which an event was fired
-     *
-     * @throws NullPointerException if <code>fullyQualifiedName</code> or <code>selector</code> is null.
+    /*
+     * The fully qualified name of a class that can rerun the suite or test
+     *     about which an event was fired. This method should never return <code>null</code>.
      */
-    public Event(String fullyQualifiedName, boolean isModule, Selector selector) {
-        if (fullyQualifiedName == null) {
-            throw new NullPointerException("fullyQualifiedName was null");
-        }
-        if (selector == null) {
-            throw new NullPointerException("selector was null");
-        }
-        this.fullyQualifiedName = fullyQualifiedName;
-        this.isModule = isModule;
-        this.selector = selector;
-    }
-
-    /**
-     * The fully qualified test class name about which an event was fired.
-     *
-     * @return the fully qualified test class name
-     */
-    public String getFullyQualifiedName() {
-        return fullyQualifiedName;
-    }
+    String fullyQualifiedName();
 
     /**
      * Indicates whether the fullyQualifiedName refers to a module (singleton object). If so, the
-     *          fullyQualifiedName parameter does not include the trailing dollar sign.
-     *
-     * @return true if the fullyQualifiedName refers to a module instead of a class
+     *          fullyQualifiedName string does not include the trailing dollar sign.
      */
-    public boolean isModule() {
-        return isModule;
-    }
+    boolean isModule();
+
+    /*
+     * Additional information identifying the suite or test about which an event was fired. This
+     * method should never return <code>null</code>.
+     */
+    Selector selector();
 
     /**
-     * A <code>Selector</code> that can be used to identify the suite or test about which an event was fired.
-     *
-     * @return the fully qualified test class name
+     * Indicates whether the event represents a test failure, error, success, or skip. This method should
+     * never return <code>null</code>.
      */
-    public Selector getSelector() {
-        return selector;
-    }
+    Status status();
+
+    /*
+     * A <code>Throwable</code> associated with this <code>Event</code>,
+     *     or <code>null</code> if no <code>Throwable</code> is associated with this event.
+     */
+    Throwable throwable();
 }
 
